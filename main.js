83f80f42e840219a4d2c76e591b145a940a51d9b
asthesus <asthesus@gmail.com>
2021-07-26 00:12:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-const tocipher_div = document.getElementById(`tocipher`);
-const key_div = document.getElementById(`key`);
-const ciphered_div = document.getElementById(`ciphered`);
-tocipher_div.textContent = 'Lorem ipsum curae magna venenatis mattis, purus luctus cubilia quisque in et, leo enim aliquam consequat.';
-tocipher_div.addEventListener('input', () => {cipher((tocipher_div.textContent), (key_div.textContent), 1)});
-key_div.addEventListener('input', () => {cipher((tocipher_div.textContent), (key_div.textContent), 1)});
-function cipher(tocipher, key, direction) {
-    if(tocipher.length !== 0 && key.length !== 0) {
-        n2 = 0;
-        ciphered = ``;
-        for(n1 = 0; n1 <= tocipher.length - 1; n1++) {
-            cipher_char = tocipher.codePointAt(n1) + (direction * key.codePointAt(n2));
-            for(;cipher_char > 65535; cipher_char -= 65535);
-            for(;cipher_char < 0; cipher_char += 65535);
-            ciphered = ciphered.concat(String.fromCodePoint(cipher_char));
-            if(n2 + 1 > key.length - 1) {n2 = 0} else n2++;
-        }
-        ciphered_div.textContent = ciphered;
-        console.log(ciphered);
-        console.log((encodeURI(key_div.textContent).split(/%..|./).length - 1) * 8);
-    } 
-}
+var $jscomp=$jscomp||{};$jscomp.scope={};$jscomp.createTemplateTagFirstArg=function(a){return a.raw=a};$jscomp.createTemplateTagFirstArgWithRaw=function(a,b){a.raw=b;return a};var tocipher_div=document.getElementById("tocipher"),key_div=document.getElementById("key"),ciphered_div=document.getElementById("ciphered");tocipher_div.textContent="Lorem ipsum curae magna venenatis mattis, purus luctus cubilia quisque in et, leo enim aliquam consequat.";
+tocipher_div.addEventListener("input",function(){cipher(tocipher_div.textContent,key_div.textContent,1)});key_div.addEventListener("input",function(){cipher(tocipher_div.textContent,key_div.textContent,1)});
+function cipher(a,b,c){if(0!==a.length&&0!==b.length){n2=0;ciphered="";for(n1=0;n1<=a.length-1;n1++){for(cipher_char=a.codePointAt(n1)+c*b.codePointAt(n2);65535<cipher_char;cipher_char-=65535);for(;0>cipher_char;cipher_char+=65535);ciphered=ciphered.concat(String.fromCodePoint(cipher_char));n2+1>b.length-1?n2=0:n2++}ciphered_div.textContent=ciphered;console.log(ciphered);console.log(8*(encodeURI(key_div.textContent).split(/%..|./).length-1))}};
